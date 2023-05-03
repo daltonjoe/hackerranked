48116e45bfc3e1cc9aee6d90aceda9f62d96c7cd
talha_guzel1907@hotmail.com <talha_guzel1907@hotmail.com>
2023-05-03 21:09:35 +0300

    first commit

---
+++ Overriding.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+  
+    void get_number_of_team_members(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+    @Override
+    void get_number_of_team_members(){
+        System.out.println( "Each team has 11 players in " + getName() );
+
+}}
+
+public class Overriding{
+	
+    public static void main(String []args){
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.get_number_of_team_members();
+        System.out.println(c2.getName());
+        c2.get_number_of_team_members();
+	}
+}
